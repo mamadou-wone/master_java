cbf3b7f041694347680915f98d69969e3e52c358
Mamadou WONE <mamadu.wone@gmail.com>
2022-02-08 07:10:17 +0000

    exo16

--- src/src/Exo16/Exo16.java
+++ src/src/Exo16/Exo16.java
@@ -1,4 +1,22 @@
 package Exo16;
 
 public class Exo16 {
+
+    public static boolean isSuffix(String u, String v){
+        return u.endsWith(v);
+    }
+
+    public static String extractChain(String s, int start, int stop){
+        String chain = "";
+        if (start <= stop && start >= 0 && stop < s.length()){
+            for (int i = start; i <= stop; i++) {
+                chain += s.charAt(i);
+            }
+        }
+        return chain;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(extractChain("Mamadou Ibn Amir Boss WONE", 12, 150));
+    }
 }
